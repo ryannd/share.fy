af7575f3110d8068daa610b43d0da1bfddfccf9e
Kenlos <kendrick.cortez116@gmail.com>
2021-02-27 16:05:10 -0600

    Landing Page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import './Landing.css'
 
 export default class Landing extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Worksaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+            <div class="Login d-flex align-items-center justify-content-center flex-column">
+                <h1 class="Name">Share.fy</h1>
+                <p class="Slogan">Search, Compare, Share</p>
+            
+                <Button style={{ backgroundColor: "#32da8a" }} href = "https://accounts.spotify.com/en/login" target="_blank" >Login</Button>{' '}
+                
             </div>
         )
     }
