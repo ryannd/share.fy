c904afc5c3b8fe9277ed6810038dad38d9b4bb75
ryannd <ryandimaranan23@gmail.com>
2021-02-27 11:36:29 -0600

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Landing from './components/Landing'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
