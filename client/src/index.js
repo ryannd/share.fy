ba3f2a5b46243b87189e6230f897ff7b0bf09b57
ryannd <ryandimaranan23@gmail.com>
2021-02-28 01:22:27 -0600

    index fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-=======
-
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 29481caca784b622b0d8c3f4bc6c7100ddd7ec1f
 
 ReactDOM.render(
   <React.StrictMode>
