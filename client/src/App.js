77509177e6ced7384ccd21da68ad95a5b7807c03
Jmal1999 <jamalstewart111@gmail.com>
2021-02-28 01:15:50 -0600

    search skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,15 @@ import {
   Route
 } from "react-router-dom";
 
-import Landing from './components/Landing'
 import Login from './components/Login/Login'
+import Searchpage from './components/Searchpage/Searchpage'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Landing}/>
         <Route exact path="/callback" component={Login}/>
+        <Route exact path ="/search" component ={Searchpage}/>
       </Switch>
     </Router>
   );
