7a909ddaeec3fecc604abd74b830ee68312da173
ryannd <ryandimaranan23@gmail.com>
2021-02-28 01:17:20 -0600

    idk

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -5,3 +5,14 @@ exports.getPlaylist = function(req,res) {
     console.log(user)
     spotifyApi.getUserPlaylists(user).then((data) => res.status(200).send(data)).catch((err) => console.log(err))
 }
+
+exports.getUser = function(req,res) {
+    let user = req.body.user
+    spotifyApi.getUser(user).then((data) => res.status(200).send(data)).catch((err) => console.log(err))
+}
+
+exports.getTracks = function(req,res) {
+    let tracks = req.body.tracks
+    spotifyApi.getTracks(tracks).then((data) => res.status(200).send(data)).catch((err) => console.log(err))
+    
+}
--- routes/search.js
+++ routes/search.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var searchController = require('../controllers/searchController')
 
 router.post('/user-playlists', searchController.getPlaylist)
+router.post('/user', searchController.getUser)
+router.post('/tracks', searchController.getTracks)
 
 module.exports = router
